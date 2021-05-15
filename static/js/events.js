a09670d00d1113cde549d296ee24610d9bb235da
Alberto Strappazzon <a@strappazzon.xyz>
2021-05-15 17:56:24 +0200

    Add CTRL+S keyboard shortcut

--- static/js/events.js
+++ static/js/events.js
@@ -30,3 +30,12 @@ $("#embed-paste").click(() => {
 $("#edit").click(() => {
 	openInNewTab();
 });
+
+// Generate URL when pressing CTRL+S
+$('body').on('keydown', e => {
+	// https://stackoverflow.com/a/27278604
+	if ((e.metaKey || e.ctrlKey) && (e.key === 's')) {
+		e.preventDefault();
+		generateLink('url');
+	}
+});
